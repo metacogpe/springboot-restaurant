bea7f433441d4f549f3eaab72ef20d5cd448ff6d
metacog <metacog@kakao.com>
2022-06-28 22:38:03 +0900

    application.yaml에 설정한 정보값 가져오기 어노테이션

---
+++ src/main/java/com/example/restaurant/naver/NaverClient.java
@@ -0,0 +1,20 @@
+package com.example.restaurant.naver;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NaverClient {
+
+    @Value("${naver.client.id}")  // application.yaml에 설정 정보 값 가져오는 어노테이션
+    private String naverClientId;
+
+    @Value("${naver.client.secret}")
+    private String naverClientSecret;
+
+    @Value("${naver.url.search.local}")
+    private String naverLocalSearchUrl;
+
+    @Value("${naver.url.search.image}")
+    private String naverImageSearchUrl;
+}
