e8cfa4ad16fbf61c9314a4c2acd031ea7ba13a10
metacog <metacog@kakao.com>
2022-07-03 00:03:17 +0900

    네이버 클라이언트 요청 주소 만들기와 헤더 준비, 준비한 헤더를 entity에 담기 등 & 테스트

--- src/main/java/com/example/restaurant/naver/NaverClient.java
+++ src/main/java/com/example/restaurant/naver/NaverClient.java
@@ -1,9 +1,18 @@
 package com.example.restaurant.naver;
 
 import com.example.restaurant.naver.dto.SearchLocalReq;
+import com.example.restaurant.naver.dto.SearchLocalRes;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.awt.*;
 
 @Component
 public class NaverClient {
@@ -20,13 +29,34 @@ public class NaverClient {
     @Value("${naver.url.search.image}")
     private String naverImageSearchUrl;
 
-    public void localSearch(SearchLocalReq searchLocalReq){
-        // 요청 보내는 방법
+    public SearchLocalRes localSearch(SearchLocalReq searchLocalReq){
+        // 요청 주소 만들기
         var uri = UriComponentsBuilder.fromUriString(naverLocalSearchUrl)
                 .queryParams(searchLocalReq.toMultiValueMap())
                 .build()
                 .encode()
                 .toUri();
+        // 헤더 준비
+        var headers = new HttpHeaders();
+        headers.set("X-Naver-Client-Id", naverClientId);
+        headers.set("X-Naver-Client-Secret", naverClientSecret);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        // 준비한 헤더를 Entity에 담기
+        var httpEntity = new HttpEntity<>(headers);
+        // response타입 정의
+        var responseType = new ParameterizedTypeReference<SearchLocalRes>(){};
+
+        // rest template 통해서 받아 주기만 하면 됨
+        var responseEntity = new RestTemplate().exchange(
+                uri,
+                HttpMethod.GET,
+                httpEntity,
+                responseType
+        );
+        return responseEntity.getBody();   // SearchLocalRes 타입으로 getBody()를 통해서 리턴
+
+
     }
 
     public void imageSearch(){
